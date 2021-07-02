61f9e741afd27a28e1bcc44d3f742273c3c868ba
rafael747cardoso <rafael747cardoso@gmail.com>
2021-07-02 20:48:09 -0300

    grouped bar matplotlib ok

--- Plot_types/Barplot_Grouped/Barplot_Grouped_Python_matplotlib.py
+++ Plot_types/Barplot_Grouped/Barplot_Grouped_Python_matplotlib.py
@@ -22,11 +22,11 @@ df = pd.read_csv(path_data + "nasa_exoplanets.csv")
 df_varnames = pd.read_csv(path_data + "nasa_exoplanets_var_names.csv")
 
 # Variables:
-cat_var1 = "discoverymethod"
-cat_var2 = "pl_tsystemref"
+cat_var1 = "pl_letter"
+cat_var2 = "discoverymethod"
 cat_var_name1 = df_varnames.loc[(df_varnames["var"] == cat_var1), ["var_name"]].values[0][0]
 cat_var_name2 = df_varnames.loc[(df_varnames["var"] == cat_var2), ["var_name"]].values[0][0]
-cat_vars_name = cat_var_name2 + " grouped by " + cat_var_name1
+cat_vars_name = cat_var_name1 + " grouped by " + cat_var_name2
 variables = [cat_var1, cat_var2]
 
 # Deal with NAN:
@@ -39,78 +39,45 @@ df_plot = pd.DataFrame({cat_var2: levels_var2})
 for lvl_var1 in levels_var1:
     var2invar1 = df.loc[df[cat_var1] == lvl_var1][cat_var2].tolist()
     var2invar1_counts = [var2invar1.count(i) if var2invar1.count(i) > 0 else np.nan for i in levels_var2]
-    var2invar1_counts_rel = [round(i/np.nansum(var2invar1_counts)*100, 2) for i in var2invar1_counts]
-    var2invar1_counts_rel_char = [np.nan if np.isnan(i) else str(i) + "%" for i in var2invar1_counts_rel]
     lvl_var1_name = re.sub(pattern = r"[ \-()#/@;:<>{}=~|.?,\\]", 
                            repl = "_", 
                            string = lvl_var1)
     df_plot = pd.concat(
         [
             df_plot,
-            # Absolute frequency:
-            pd.DataFrame({lvl_var1_name + "_freq_abs": var2invar1_counts},
-                         dtype = "int"),
-            # Relative frequency:
-            pd.DataFrame({lvl_var1_name + "_freq_rel": var2invar1_counts_rel},
-                         dtype = "int"),
-            pd.DataFrame({lvl_var1_name + "_freq_rel_char": var2invar1_counts_rel_char},
+            pd.DataFrame({lvl_var1_name: var2invar1_counts},
                          dtype = "int")
         ],
         axis = 1)
-
-
-interest_columns = [i for i in df_plot.columns.tolist() if re.search(pattern = "freq_abs", string = i) or 
-                                                           re.search(pattern = cat_var2, string = i)]
-df_plot = df_plot[interest_columns]
+df_plot = df_plot.fillna(0)
 
 # Plot:
-
-# df_plot = df_plot.iloc[:, :7]
-
-n_levels = df_plot.shape[0]
+n_groups = df_plot.shape[1] - 1
 cmap = LinearSegmentedColormap.from_list("my_palette", ["#111539", "#97A1D9"])
-my_palette = [cmap(i/n_levels) for i in np.array(range(n_levels))]
-
-n_bars = n_levels
-total_width = 0.8
-single_width = 0.9
-bar_width = total_width/n_bars
-
+my_palette = [cmap(i/n_groups) for i in np.array(range(n_groups))]
 fig, ax = plt.subplots(
     figsize = (20, 10),
     tight_layout = True
 )
-
+n_bars = n_groups
+total_width = 0.8
+single_width = 0.9
+bar_width = total_width/n_groups
 bars = []
-for i, (name, values) in enumerate(df_plot.items()):
-    x_offset = (i - n_bars / 2) * bar_width + bar_width / 2
+for i, (name, values) in enumerate(df_plot.iloc[:, 1:].items()):
+    x_offset = (i - n_bars/2)*bar_width + bar_width/2
     for x, y in enumerate(values):
         bar = ax.bar(
             x = x + x_offset, 
-            y = y, 
-            width = bar_width * single_width, 
+            height = y, 
+            width = bar_width*single_width, 
             color = my_palette[i]
         )
     bars.append(bar[0])
-    ax.legend(bars, df_plot.keys())
-
-for lvl_var1 in levels_var1[0:1]:
-    lvl_var1 = levels_var1[0]
-    lvl_var1_name = re.sub(pattern = r"[ \-()#/@;:<>{}=~|.?,\\]",
-                           repl = "_",
-                           string = lvl_var1)
-    _ = ax.bar(
-        x = x_location + sinal*bar_width/2,
-        height = df_plot[lvl_var1_name + "_freq"],
-        color = my_palette
-    )
-    sinal = -sinal
-    ax.bar_label(_, padding = 3)
-
-
-
-
-_ = ax.set_yscale("linear")
+    ax.legend(bars, df_plot.iloc[:, 1:].keys())
+_ = ax.set_xticks(np.arange(df_plot.shape[0]))
+_ = ax.set_xticklabels(df_plot[cat_var2])
+_ = ax.set_yscale("log")
 _ = ax.set_xlabel(
     cat_vars_name,
     fontsize = 16
@@ -130,9 +97,8 @@ _ = ax.tick_params(
     which = "major", 
     labelsize = 16
 )
-
-
-
-
+_ = plt.setp(ax.get_xticklabels(),
+             ha = "right", 
+             rotation_mode = "anchor")
 
 
