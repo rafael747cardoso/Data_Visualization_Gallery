4ac945ccafebfc3f2c2e4753c607c754a6fcced3
rafael747cardoso <rafael747cardoso@gmail.com>
2021-07-10 18:18:24 -0300

    boxplot ggplot2 fixed

--- Plot_types/Boxplot/Boxplot_R_ggplot2.R
+++ Plot_types/Boxplot/Boxplot_R_ggplot2.R
@@ -38,34 +38,49 @@ sorted_levels = sort(unique(df_plot[, cat_var]))
 df_plot[, cat_var] = factor(x = df_plot[, cat_var],
                             levels = sorted_levels)
 
+
+# df_plot = df_plot %>%
+#              dplyr::filter(discoverymethod == "Transit")
+
+
+
 # Plot:
 my_palette = colorRampPalette(c("#111539", "#97A1D9"))
 outlier_color = "#DA2E2E"
 median_color = "#23C16A"
 n_levels = length(unique(df_plot[, cat_var]))
-log_scale_fix = 10
 p = ggplot(
         data = df_plot,
         aes(
             x = get(cat_var),
-            y = get(num_var)*log_scale_fix,
+            y = get(num_var),
             fill = get(cat_var),
-            color = get(cat_var)
+            color = get(cat_var),
+            stat = "identity"
         )
     ) +
     stat_boxplot(
         geom = "errorbar",
-        show.legend = FALSE
+        show.legend = FALSE,
+        width = 0.6
     ) +
     geom_boxplot(
         outlier.colour = outlier_color,
         outlier.alpha = 0.5,
-        show.legend = FALSE
+        show.legend = FALSE,
+        width = 0.6
+    ) +
+    coord_trans(
+        y = "log10"
+    ) +
+    scale_y_continuous(
+        breaks = breaks,
+        limits = c(1, max(df_plot[, num_var], na.rm = TRUE))
     ) +
     stat_summary(
         geom = "crossbar",
         color = median_color,
-        width = 0.75,
+        width = 0.6,
         fatten = 2,
         fun.data = function(x){c(y = median(x),
                                  ymin = median(x),
@@ -77,10 +92,6 @@ p = ggplot(
     ) +
     scale_color_manual(
         values = my_palette(n_levels)
-    ) +
-    scale_y_continuous(
-        labels = function(x){x/log_scale_fix},
-        trans = "log10"
     ) +
     theme(
         axis.text.x = element_text(
