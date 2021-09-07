8b79891835691dd2268a447199bedbe8d36e8983
rafael747cardoso <rafael747cardoso@gmail.com>
2021-09-07 20:56:07 -0300

    hist 1D seaborn

--- Plot_types/Histogram/Histogram_1D_Overlaid/Histogram_1D_Overlaid_Python_seaborn.py
+++ Plot_types/Histogram/Histogram_1D_Overlaid/Histogram_1D_Overlaid_Python_seaborn.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 # Display options:
 pd.set_option("display.width", 1200)
@@ -20,21 +21,30 @@ df_varnames = pd.read_csv(path_data + "nasa_exoplanets_var_names.csv")
 # Variables:
 x_var = "sy_bmag"
 x_var_name = df_varnames.loc[(df_varnames["var"] == x_var), ["var_name"]].values[0][0]
+color_var = "disc_locale"
+color_var_name = df_varnames.loc[(df_varnames["var"] == color_var), ["var_name"]].values[0][0]
 
 # Adapt the data:
-x_vals = df[x_var]
+df_plot = df[[x_var, color_var]]
 
 # Plot:
+lvls = df_plot[color_var].unique()
+n_levels = len(lvls)
+cmap = LinearSegmentedColormap.from_list("my_palette", ["#111539", "#97A1D9"])
+my_palette = [cmap(i/n_levels) for i in np.array(range(n_levels))]
+
 fig = plt.figure(
     figsize = (20, 10),
     tight_layout = True
 )
 ax = sns.histplot(
-    x = x_vals,
+    data = df_plot,
+    x = x_var,
+    hue = color_var,
+    palette = my_palette,
     bins = 100,
     stat = "count",
-    color = "#813DDA",
-    alpha = 0.9,
+    alpha = 0.7,
     kde = False
 )
 plt.rcParams["patch.edgecolor"] = "none"
@@ -57,9 +67,5 @@ _ = ax.tick_params(
     axis = "y", 
     which = "major", 
     labelsize = 16
-)
-plt.legend(
-    [], [], 
-    frameon = False
 )
 
