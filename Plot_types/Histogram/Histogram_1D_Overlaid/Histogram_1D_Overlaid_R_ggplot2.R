61710ae63a7824ec5c137377f497a94a65f820b4
rafael747cardoso <rafael747cardoso@gmail.com>
2021-09-07 20:36:17 -0300

    new plots

---
+++ Plot_types/Histogram/Histogram_1D_Overlaid/Histogram_1D_Overlaid_R_ggplot2.R
@@ -0,0 +1,80 @@
+
+# Paths:
+path_data = "data/"
+path_lib = "renv/library/R-4.1/x86_64-pc-linux-gnu/"
+
+# Packages:
+require(dplyr, lib = path_lib)
+require(readr, lib = path_lib)
+require(RColorBrewer, lib = path_lib)
+require(ggplot2, lib = path_lib)
+
+# Dataset:
+df = readr::read_csv(paste0(path_data, "nasa_exoplanets.csv")) %>%
+         as.data.frame()
+attr(df, "spec") = NULL
+df_varnames = readr::read_csv(paste0(path_data, "nasa_exoplanets_var_names.csv")) %>%
+                  as.data.frame()
+attr(df_varnames, "spec") = NULL
+
+# Variables:
+x_var = "sy_bmag"
+x_var_name = (df_varnames %>%
+                 dplyr::filter(var == x_var))$var_name
+
+# Adapt the data:
+x_vals = df[, x_var]
+
+# Plot:
+p = ggplot() + 
+    geom_histogram(
+        aes(
+            x = x_vals
+        ),
+        stat = "bin",
+        bins = 100,
+        color = "#813DDA",
+        fill = "#813DDA",
+        show.legend = FALSE
+    ) +
+    theme(
+        axis.text.x = element_text(
+            size = 14,
+            angle = 0, 
+            hjust = 1,
+            vjust = 1
+        ),
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(
+            size = 15,
+            face = "bold"
+        ),
+        axis.title.y = element_text(
+            size = 15,
+            face = "bold"
+        ),
+        panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(
+            size = 0.2,
+            linetype = "solid",
+            colour = "#eaeaea"
+        ),
+        panel.grid.minor = element_line(
+            size = 0.1,
+            linetype = "solid",
+            colour = "#eaeaea"
+        ),
+        plot.margin = margin(
+            t = 0, 
+            r = 5,
+            b = 5, 
+            l = 10,
+            unit = "pt"
+        )
+    ) +
+    xlab(x_var_name) +
+    ylab("Counts")
+
+p
+
+
