ed3d7431344a00c4ba3563a08778927332523222
rafael747cardoso <rafael747cardoso@gmail.com>
2021-09-08 01:36:17 -0300

    hist 2D python plotly

--- Plot_types/Histogram/Histogram_2D/Histogram_2D_Python_plotly.py
+++ Plot_types/Histogram/Histogram_2D/Histogram_2D_Python_plotly.py
@@ -1,3 +1,4 @@
+
 # Paths:
 path_data = "data/"
 
@@ -5,6 +6,7 @@ path_data = "data/"
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+from matplotlib.colors import LinearSegmentedColormap, to_hex
 
 # Display options:
 pd.set_option("display.width", 1200)
@@ -16,35 +18,50 @@ df = pd.read_csv(path_data + "nasa_exoplanets.csv")
 df_varnames = pd.read_csv(path_data + "nasa_exoplanets_var_names.csv")
 
 # Variables:
-x_var = "sy_bmag"
+x_var = "sy_vmag"
 x_var_name = df_varnames.loc[(df_varnames["var"] == x_var), ["var_name"]].values[0][0]
+y_var = "sy_jmag"
+y_var_name = df_varnames.loc[(df_varnames["var"] == y_var), ["var_name"]].values[0][0]
 
 # Adapt the data:
-x_vals = df[x_var]
+df_plot = df[[x_var, y_var]]
+
+# Deal with nan:
+df_plot = df_plot.dropna()
 
 # Plot:
+n_binsxy = 150
+cmap = LinearSegmentedColormap.from_list("my_palette",
+                                         ["#000000", "#E008F8", "#F81D08", "#F88A08", "#F7FE04"])
+my_palette = [to_hex(j) for j in  [cmap(i/n_binsxy) for i in np.array(range(n_binsxy))]]
+
 fig = go.Figure(
     data = [
-        go.Histogram(
-            x = x_vals,
+        go.Histogram2d(
+            x = df_plot[x_var],
+            y = df_plot[y_var],
             histfunc = "count",
-            nbinsx = 100,
-            marker_color = "#813DDA",
-            opacity = 0.9
+            nbinsx = n_binsxy,
+            nbinsy = n_binsxy,
+            colorscale = my_palette,
+            hovertemplate = "<b>" +
+                            x_var_name + ": %{x:,}<br>" +
+                            y_var_name + ": %{y:,}</br>" +
+                            "Counts: %{z:, }</b><extra></extra>",
+            colorbar = dict(
+                title = "<b>Counts</b>"
+            )
         )
     ]
 )
-fig.update_traces(
-    hovertemplate = "<b>Counts: %{y:,}<br>" + 
-                    x_var_name + ": %{x:,}</b><extra></extra>"
-)
 fig.update_layout(
+    height = 800,
+    width = 800,
     xaxis_title = "<b>" + x_var_name + "</b>" ,
-    yaxis_title = "<b>Counts</b>",
+    yaxis_title = "<b>" + y_var_name + "</b>",
     font = dict(
         size = 18
     ),
-    showlegend = False,
     plot_bgcolor = "white",
     hoverlabel = dict(
         font_size = 18,
