6018a0c8ec530727d902da96fdc56c5c63795c61
rafael747cardoso <rafael747cardoso@gmail.com>
2021-09-08 01:19:39 -0300

    hist 2D seaborn

--- Plot_types/Histogram/Histogram_2D/Histogram_2D_Python_seaborn.py
+++ Plot_types/Histogram/Histogram_2D/Histogram_2D_Python_seaborn.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 # Display options:
 pd.set_option("display.width", 1200)
@@ -18,33 +19,45 @@ df = pd.read_csv(path_data + "nasa_exoplanets.csv")
 df_varnames = pd.read_csv(path_data + "nasa_exoplanets_var_names.csv")
 
 # Variables:
-x_var = "sy_bmag"
+x_var = "sy_vmag"
 x_var_name = df_varnames.loc[(df_varnames["var"] == x_var), ["var_name"]].values[0][0]
+y_var = "sy_jmag"
+y_var_name = df_varnames.loc[(df_varnames["var"] == y_var), ["var_name"]].values[0][0]
 
 # Adapt the data:
-x_vals = df[x_var]
+df_plot = df[[x_var, y_var]]
+
+# Deal with nan:
+df_plot = df_plot.dropna()
 
 # Plot:
-fig = plt.figure(
-    figsize = (20, 10),
+n_binsxy = 150
+cmap = LinearSegmentedColormap.from_list("my_palette",
+                                         ["#000000", "#E008F8", "#F81D08", "#F88A08", "#F7FE04"])
+my_palette = [cmap(i/n_binsxy) for i in np.array(range(n_binsxy))]
+
+fig, ax = plt.subplots(
+    figsize = (11, 10),
     tight_layout = True
 )
-ax = sns.histplot(
-    x = x_vals,
-    bins = 100,
+_ = sns.histplot(
+    data = df_plot,
+    x = x_var,
+    y = y_var,
     stat = "count",
-    color = "#813DDA",
-    alpha = 0.9,
-    kde = False
+    bins = (n_binsxy, n_binsxy),
+    legend = True,
+    cbar = True,
+    ax = ax
 )
-plt.rcParams["patch.edgecolor"] = "none"
+_ = ax.collections[0].colorbar.set_label("Counts")
 _ = ax.set_xlabel(
     x_var_name,
     fontsize = 16,
     fontweight = "bold"
 )
 _ = ax.set_ylabel(
-    "Counts",
+    y_var_name,
     fontsize = 16,
     fontweight = "bold"
 )
@@ -58,8 +71,5 @@ _ = ax.tick_params(
     which = "major", 
     labelsize = 16
 )
-plt.legend(
-    [], [], 
-    frameon = False
-)
+
 
