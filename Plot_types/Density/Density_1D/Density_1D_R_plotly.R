d367796be160253a33288c556b0712f95e9a33b3
rafael747cardoso <rafael747cardoso@gmail.com>
2021-09-13 22:37:39 -0300

    density 1D r plotly

---
+++ Plot_types/Density/Density_1D/Density_1D_R_plotly.R
@@ -0,0 +1,71 @@
+
+# Paths:
+path_data = "data/"
+path_lib = "renv/library/R-4.1/x86_64-pc-linux-gnu/"
+
+# Packages:
+require(dplyr, lib = path_lib)
+require(readr, lib = path_lib)
+require(RColorBrewer, lib = path_lib)
+require(plotly, lib = path_lib)
+
+# Dataset:
+df = readr::read_csv(paste0(path_data, "nasa_exoplanets.csv")) %>%
+         as.data.frame()
+attr(df, "spec") = NULL
+df_varnames = readr::read_csv(paste0(path_data, "nasa_exoplanets_var_names.csv")) %>%
+                  as.data.frame()
+attr(df_varnames, "spec") = NULL
+
+# Variables:
+x_var = "sy_bmag"
+x_var_name = (df_varnames %>%
+                 dplyr::filter(var == x_var))$var_name
+
+# Deal with NA:
+df = df[!is.na(df[, x_var]), ]
+
+# Adapt the data:
+dens = density(df[, x_var])
+
+# Plot:
+p = plot_ly(
+    x = dens$x,
+    y = dens$y,
+    type = "scatter",
+    mode = "lines",
+    fill = "tozeroy",
+    color = "#813DDA",
+    colors = "#813DDA",
+    opacity = 0.9,
+    hovertemplate = paste0("<b>", x_var_name, ": %{x:,}<br>",
+                           "Density: %{y:,}</b><extra></extra>")
+) %>%
+    layout(
+        xaxis = list(
+            title = paste0("<b>", x_var_name, "</b>"),
+            titlefont = list(size = 20),
+            tickfont = list(size = 18),
+            categoryorder = "array"
+        ),
+        yaxis = list(
+            title = paste0("<b>Density</b>"),
+            titlefont = list(size = 20),
+            tickfont = list(size = 18)
+        ),
+        margin = list(
+            l = 10,
+            r = 10,
+            t = 10,
+            b = 10
+        ),
+        hoverlabel = list(font = list(size = 18)),
+        showlegend = FALSE
+    )
+
+p
+
+
+
+
+
