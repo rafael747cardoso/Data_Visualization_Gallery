de0b4809cd2b31ae76ba056ec242733d0ca3bdaa
rafael747cardoso <rafael747cardoso@gmail.com>
2021-10-01 16:49:47 -0300

    parallel matplotlib going

--- Plot_types/Parallel/Parallel_Python_matplotlib.py
+++ Plot_types/Parallel/Parallel_Python_matplotlib.py
@@ -20,26 +20,25 @@ pd.set_option("display.max_rows", 300)
 df = pd.read_csv(path_data + "nasa_exoplanets.csv")
 df_varnames = pd.read_csv(path_data + "nasa_exoplanets_var_names.csv")
 
-# Variables:
-# my_vars = ["pl_letter", "discoverymethod", "disc_locale", "pl_orbper", "st_teff",
-#                "st_metratio", "sy_gaiamag", "dec"]
-my_vars = ["pl_letter", "pl_orbper", "st_teff", "disc_locale", "sy_gaiamag"]
+# Variables (the first variable must be categoric):
+my_vars = ["discoverymethod", "pl_orbper", "st_teff", "disc_locale", "sy_gaiamag"]
 my_vars_names = reduce(pd.DataFrame.append,
                        map(lambda i: df_varnames[df_varnames["var"] == i], my_vars))
 my_vars_names = my_vars_names["var_name"].values.tolist()
 
 # Adapt the data:
+df = df.loc[df["pl_letter"] == "d"]
 df_plot = df[my_vars]
 df_plot = df_plot.dropna()
 df_plot = df_plot.reset_index(drop = True)
 
-df_plot = df_plot.iloc[25150:25184,:]
-
 # Convert to numeric matrix:
 ym = []
+dics_vars = []
 for v, var in enumerate(my_vars):
     if df_plot[var].dtype.kind not in ["i", "u", "f"]:
         dic_var = dict([(val, c) for c, val in enumerate(df_plot[var].unique())])
+        dics_vars += [dic_var]
         ym += [[dic_var[i] for i in df_plot[var].tolist()]]
     else:
         ym += [df_plot[var].tolist()]
@@ -52,7 +51,7 @@ dys = ymaxs - ymins
 ymins -= dys*0.05
 ymaxs += dys*0.05
 
-# Reverse some axes:
+# Reverse some axes for better visual:
 axes_to_reverse = []
 for a in axes_to_reverse:
     ymaxs[a], ymins[a] = ymins[a], ymaxs[a]
@@ -64,7 +63,7 @@ zs[:, 0] = ym[:, 0]
 zs[:, 1:] = (ym[:, 1:] - ymins[1:])/dys[1:]*dys[0] + ymins[0]
 
 # Colors:
-n_levels = 100
+n_levels = len(dics_vars[0])
 my_colors = ["#F41E1E", "#F4951E", "#F4F01E", "#4EF41E", "#1EF4DC", "#1E3CF4", "#F41EF3"]
 cmap = LinearSegmentedColormap.from_list("my_palette", my_colors)
 my_palette = [cmap(i/n_levels) for i in np.array(range(n_levels))]
@@ -110,100 +109,33 @@ host_ax.tick_params(
 )
 host_ax.spines.right.set_visible(False)
 host_ax.xaxis.tick_top()
-
-colors = plt.cm.viridis.colors
-legend_handles = [None for _ in my_vars_names]
 for j in range(ym.shape[0]):
     verts = list(zip([x for x in np.linspace(0, len(ym) - 1, len(ym)*3 - 2, 
                                              endpoint = True)],
                  np.repeat(zs[j, :], 3)[1: -1]))
     codes = [Path.MOVETO] + [Path.CURVE4 for _ in range(len(verts) - 1)]
     path = Path(verts, codes)
+    color_first_cat_var = my_palette[dics_vars[0][df_plot.iloc[j, 0]]]
     patch = patches.PathPatch(
         path,
         facecolor = "none",
         lw = 2,
-        alpha = 0.7#,
-        # edgecolor = colors[my_vars[j]]
+        alpha = 0.7,
+        edgecolor = color_first_cat_var
     )
-    # legend_handles[my_vars[j]] = patch
     host_ax.add_patch(patch)
-host_ax.legend(
-    legend_handles,
-    my_vars_names,
-    loc = "lower center",
-    bbox_to_anchor = (0.5, -0.18),
-    ncol = len(my_vars_names),
-    fancybox = True,
-    shadow = True
-)
 
-
-
-
-
-
-
-
-
-
-from sklearn import datasets
-iris = datasets.load_iris()
-ynames = iris.feature_names
-ys = iris.data
-ymins = ys.min(axis=0)
-ymaxs = ys.max(axis=0)
-dys = ymaxs - ymins
-ymins -= dys * 0.05  # add 5% padding below and above
-ymaxs += dys * 0.05
-
-axes_to_reverse = [1,3, 2]
-for a in axes_to_reverse:
-    ymaxs[a], ymins[a] = ymins[a], ymaxs[a]
-dys = ymaxs - ymins
-
-# transform all data to be compatible with the main axis
-zs = np.zeros_like(ys)
-zs[:, 0] = ys[:, 0]
-zs[:, 1:] = (ys[:, 1:] - ymins[1:]) / dys[1:] * dys[0] + ymins[0]
-
-fig, host = plt.subplots(figsize=(10,4))
-
-axes = [host] + [host.twinx() for i in range(ys.shape[1] - 1)]
+dic_count = 0
 for i, ax in enumerate(axes):
-    ax.set_ylim(ymins[i], ymaxs[i])
-    ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    if ax != host:
-        ax.spines['left'].set_visible(False)
-        ax.yaxis.set_ticks_position('right')
-        ax.spines["right"].set_position(("axes", i / (ys.shape[1] - 1)))
-
-host.set_xlim(0, ys.shape[1] - 1)
-host.set_xticks(range(ys.shape[1]))
-host.set_xticklabels(ynames, fontsize=14)
-host.tick_params(axis='x', which='major', pad=7)
-host.spines['right'].set_visible(False)
-host.xaxis.tick_top()
-host.set_title('Parallel Coordinates Plot — Iris', fontsize=18, pad=12)
-
-colors = plt.cm.Set2.colors
-legend_handles = [None for _ in iris.target_names]
-for j in range(ys.shape[0]):
-    # create bezier curves
-    verts = list(zip([x for x in np.linspace(0, len(ys) - 1, len(ys) * 3 - 2, endpoint=True)],
-                     np.repeat(zs[j, :], 3)[1:-1]))
-    codes = [Path.MOVETO] + [Path.CURVE4 for _ in range(len(verts) - 1)]
-    path = Path(verts, codes)
-    patch = patches.PathPatch(path, facecolor='none', lw=2, alpha=0.7, edgecolor=colors[iris.target[j]])
-    legend_handles[iris.target[j]] = patch
-    host.add_patch(patch)
-host.legend(legend_handles, iris.target_names,
-            loc='lower center', bbox_to_anchor=(0.5, -0.18),
-            ncol=len(iris.target_names), fancybox=True, shadow=True)
-plt.tight_layout()
-
-
+    if df_plot.iloc[:, i].dtype.kind not in ["i", "u", "f"]:
+        dic_var_i = dics_vars[dic_count]
+        ax.set_yticks(
+            range(len(dic_var_i))
+        )
+        ax.set_yticklabels(
+            [key_val for key_val in dics_vars[dic_count].keys()]
+        )
+        dic_count += 1
 
 
 
